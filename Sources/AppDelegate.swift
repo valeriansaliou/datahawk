d8e17b5c4061dfa2401c4d81a65f759dfb7e66c6
Valerian Saliou <valerian@valeriansaliou.name>
2026-05-04 09:11:23 +0200

    fix: add missing keyboard shortcuts to add hotspot form

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -59,7 +59,10 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
         mainMenu.addItem(editItem)
         let editMenu = NSMenu(title: "Edit")
         editItem.submenu = editMenu
+        editMenu.addItem(withTitle: "Cut", action: #selector(NSText.cut(_:)), keyEquivalent: "x")
+        editMenu.addItem(withTitle: "Copy", action: #selector(NSText.copy(_:)), keyEquivalent: "c")
         editMenu.addItem(withTitle: "Paste", action: #selector(NSText.paste(_:)), keyEquivalent: "v")
+        editMenu.addItem(withTitle: "Select All", action: #selector(NSText.selectAll(_:)), keyEquivalent: "a")
 
         NSApp.mainMenu = mainMenu
     }
